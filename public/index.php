ee4fc357ae78c4df1098ab3924ef7dfc4750d9ba
10tek <52250820+10tek@users.noreply.github.com>
2020-11-08 20:17:19 +0600

    Add files via upload

---
+++ public/index.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\Framework\Kernel\Application;
+use Laminas\Diactoros\ServerRequestFactory;
+use Laminas\HttpHandlerRunner\Emitter\SapiEmitter;
+
+require_once __DIR__ . "/../vendor/autoload.php";
+
+$request = ServerRequestFactory::fromGlobals();
+
+$response = Application::app($request)
+    ->start();
+
+
+(new SapiEmitter)->emit($response);
